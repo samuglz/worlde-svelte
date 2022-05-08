986e21ad5d6156020d31d86a705426c08c09241f
Samuel Gonzalez <samuel.gonzalez@eydevelopment.com>
2022-05-08 09:50:45 +0100

    feature: main app

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+import { onMount} from "svelte";
+import { getDictionary} from "../API";
+import Header from '../components/Header.svelte';
+import Grid from "../components/Grid.svelte";
+let dictionary = [];
+let word = ''
+let splitWord = [];
+let isLoading = false;
+
+onMount(async () => {
+    try {
+        isLoading = true;
+        dictionary = await getDictionary();
+        const randomNumber = Math.floor(Math.random() * dictionary.length);
+        word = dictionary[randomNumber];
+        splitWord = word.split('');
+    } catch (error) {
+        console.error(error);
+    } finally {
+        isLoading = false;
+    }
+})
+</script>
+
+<Header title={'WORDLE'} />
+{#if !isLoading}
+    <div class="mt-10">
+        <Grid />
+    </div>
+{/if}
+
