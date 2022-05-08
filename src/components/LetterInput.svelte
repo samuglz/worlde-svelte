5d542bbc0ac073538803037b35f2d51a80100e76
Samuel Gonzalez <samuel.gonzalez@eydevelopment.com>
2022-05-08 18:06:13 +0100

    feature(Letter Input component): letter display at upper case

--- src/components/LetterInput.svelte
+++ src/components/LetterInput.svelte
@@ -3,5 +3,9 @@
 </script>
 
 <div class="w-14 h-14 border-2 border-solid border-gray-300 text-2xl font-bold flex justify-center items-center">
-    {letter ? letter : ''}
+    {letter ? letter.toUpperCase() : ''}
 </div>
+
+<style>
+
+</style>
