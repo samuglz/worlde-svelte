30c6d99566e26f9c8e9d8d855b9823d12c16eb96
Samuel Gonzalez <samuel.gonzalez@eydevelopment.com>
2022-05-08 21:23:28 +0100

    style(Header component): rename classes

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,13 @@
-<h1 class="flex justify-center items-center text-3xl py-4 font-semibold w-full border-b border-solid border-gray-300">
+<h1 class="header">
     {title}
 </h1>
 
 <script>
     export let title;
 </script>
+
+<style>
+    .header {
+        @apply flex justify-center items-center text-3xl py-4 font-semibold w-full border-b border-solid border-gray-300 mb-10;
+    }
+</style>
