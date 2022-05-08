0e7307bb848d8d5c34e8b70523accd3bcfb6eb8f
Samuel Gonzalez <samuel.gonzalez@eydevelopment.com>
2022-05-08 10:38:37 +0100

    Refactor: Grid just manage the game state

--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
-import {isLetter} from "../utils";
 import Word from "./Word.svelte";
 const MAX_TRIES = 6;
-let word = []
-const handleKeydown = (event) => {
-    if(isLetter(event.key) && word.length < 5 && event.keyCode !== 8) {
-        word = [...word, event.key];
-    }
-    if(event.keyCode === 8) {
-        word = word.slice(0, word.length - 1);
-    }
+export let word = '';
+
+interface Game {
+    currentTry: number;
+    wordGuess: string;
 }
+
+const gameState: Game = {
+    currentTry: 0,
+    wordGuess: word
+}
+
 </script>
-<svelte:window on:keydown={handleKeydown}/>
+
 
 {#each Array(MAX_TRIES) as _, i}
-    <Word letters="{word}" />
+    <Word state={gameState} index={i} />
 {/each}
