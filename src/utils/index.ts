e4c8e0343eab3f4674500766a7b4527fb92171c2
Samuel Gonzalez <samuel.gonzalez@eydevelopment.com>
2022-05-08 22:05:55 +0100

    feature: add splitWord function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,4 @@ const ENTER_CODE = 13;
 export const isLetter = (char: string) => /^[a-zA-Z]$/.test(char);
 export const isBackSpace = (key: number) => key === BACKSPACE_CODE;
 export const isEnter = (key: number) => key === ENTER_CODE;
+export const splitWord = (word: string) => word.split('');
