87e3eb8c284c8a414f72c25e4e04f12751659ffb
Samuel Gonzalez <samuel.gonzalez@eydevelopment.com>
2022-05-08 09:50:12 +0100

    feature: Word component

---
+++ src/components/Word.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import LetterInput from "./LetterInput.svelte";
+    export let length = 5;
+    export let letters = [];
+</script>
+
+<div class="flex justify-center items-center gap-1 py-0.5">
+    {#each Array(length) as _, i}
+        <LetterInput letter={letters[i]} />
+    {/each}
+</div>
+
+
+
+
