635bb28f9607142fc6a26de4f8a56ff94b4ca81f
Samuel Gonzalez <samuel.gonzalez@eydevelopment.com>
2022-05-08 09:47:49 +0100

    feature: Header component

---
+++ src/components/Header.svelte
@@ -0,0 +1,7 @@
+<h1 class="flex justify-center items-center text-3xl py-4 font-semibold w-full border-b border-solid border-gray-300">
+    {title}
+</h1>
+
+<script>
+    export let title;
+</script>
