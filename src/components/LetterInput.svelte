9617b34008505f27ab399d57e9a9ab62f5632c14
Samuel Gonzalez <samuel.gonzalez@eydevelopment.com>
2022-05-08 23:23:22 +0100

    feature: add handle style

--- src/components/LetterInput.svelte
+++ src/components/LetterInput.svelte
@@ -1,11 +1,29 @@
 <script lang="ts">
  export let letter: string;
+ export let style = 'normal'
+ const handleStyle = (style) => {
+      return `cell ${style}`
+ }
 </script>
 
-<div class="w-14 h-14 border-2 border-solid border-gray-300 text-2xl font-bold flex justify-center items-center">
+<div class={handleStyle(style)}>
     {letter ? letter.toUpperCase() : ''}
 </div>
 
 <style>
-
+ .cell {
+     @apply w-14 h-14 border-2 border-solid text-2xl font-bold flex justify-center items-center;
+ }
+ .normal{
+     @apply border-gray-300 text-black;
+ }
+ .wrong {
+     @apply border-gray-500 bg-gray-500 text-white;
+ }
+ .correct {
+     @apply border-green-500 bg-green-500 text-white;
+ }
+ .partial {
+     @apply border-yellow-300 bg-yellow-300 text-white;
+ }
 </style>
