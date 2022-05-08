6cdc411f74ddbcadd9fc3118b9ed1f2e029a2a68
Samuel Gonzalez <samuel.gonzalez@eydevelopment.com>
2022-05-08 09:47:39 +0100

    feature: LetterInput component

---
+++ src/components/LetterInput.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+ export let letter: string;
+</script>
+
+<div class="w-14 h-14 border-2 border-solid border-gray-300 text-2xl font-bold flex justify-center items-center">
+    {letter ? letter : ''}
+</div>
