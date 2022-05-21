ff308c229a7a4dad0b21230d4a8082f54ca8c0cf
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2022-05-21 23:08:01 +0100

    style(Toast): refactor css

--- src/components/Toast.svelte
+++ src/components/Toast.svelte
@@ -11,5 +11,11 @@
 </script>
 
 {#if message}
-    <div class="mt-10 w-40 h-10 bg-black text-white rounded-sm flex justify-center items-center"> {message} </div>
+    <div class="toast"> {message} </div>
 {/if}
+
+<style>
+    .toast {
+        @apply mt-10 w-40 h-10 bg-black text-white rounded-sm flex justify-center items-center absolute top-10;
+    }
+</style>
