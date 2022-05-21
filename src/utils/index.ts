37d2a178f3e023bbc2c008a9a926e4a60931c690
Samuel Gonzalez <samuelgonzalezalonso@gmail.com>
2022-05-21 11:34:33 +0100

    docs(utils): type functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,8 @@
 const BACKSPACE_CODE = 'Backspace';
 const ENTER_CODE = 'Enter';
-export const isLetter = (char: string) => /^[a-zA-Z]$/.test(char);
-export const isBackSpace = (key: string) => key === BACKSPACE_CODE;
-export const isEnter = (key: string) => key === ENTER_CODE;
-export const splitWord = (word: string) => word.split('');
-export const isCorrectWord = (letterState: string[]) =>
+export const isLetter = (char: string):boolean => /^[a-zA-Z]$/.test(char);
+export const isBackSpace = (key: string):boolean => key === BACKSPACE_CODE;
+export const isEnter = (key: string):boolean => key === ENTER_CODE;
+export const splitWord = (word: string):string[] => word.split('');
+export const isCorrectWord = (letterState: string[]):boolean =>
 	letterState.every((letter) => letter === 'correct');
